876c3439a875064ec8674af6709629a31c56e0b5
Gilbb <gilbert.iskandar2002@gmail.com>
2024-05-30 17:14:14 +0800

    changed to bcryptjs

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { createClient } = require('@supabase/supabase-js')
 
 const app = express();
