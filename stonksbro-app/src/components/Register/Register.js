8a779a809de815c4e6b1d62a4b7e03ac1fc25b16
Gilbb <gilbert.iskandar2002@gmail.com>
2024-07-19 12:54:22 +0800

    Add stronger password requirement with reactive password checker

--- stonksbro-app/src/components/Register/Register.js
+++ stonksbro-app/src/components/Register/Register.js
@@ -1,15 +1,21 @@
-import "./Register.css";
-import { useState } from "react";
+import styles from "./Register.css";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import MessageBox from "../MessageBox/MessageBox";
+
+import { tokens } from "../../theme";
+import { useTheme, Box, Typography } from "@mui/material";
 
 import user_icon from "../Assets/person.png";
 import email_icon from "../Assets/email.png";
 import password_icon from "../Assets/password.png";
 import logo_icon from "../Assets/stonksBro-icon.png";
-import stock_image from "../Assets/stocksimage.jpg"; // Add your stock image path here
+import stock_image from "../Assets/stocksimage.jpg";
 
 const Register = () => {
+  const colorTheme = useTheme();
+  const colors = tokens(colorTheme.palette.mode);
+
   const navigate = useNavigate();
   const navigateToLogin = () => {
     navigate("/");
@@ -50,6 +56,16 @@ const Register = () => {
       return;
     }
 
+    if (
+      !passwordCriteria.length ||
+      !passwordCriteria.number ||
+      !passwordCriteria.specialChar ||
+      !passwordCriteria.match
+    ) {
+      setErrorMessage("Please make sure all password requirements are met.");
+      return;
+    }
+
     try {
       const response = await fetch(
         "https://stonks-bro-orbital24-server.vercel.app/register",
@@ -73,6 +89,24 @@ const Register = () => {
       setErrorMessage("An error occurred. Please try again.");
     }
   };
+
+  const [passwordCriteria, setPasswordCriteria] = useState({
+    length: false,
+    number: false,
+    specialChar: false,
+    match: false,
+  });
+
+  useEffect(() => {
+    const { password, passwordConfirm } = formData;
+
+    const length = password.length >= 8;
+    const number = /\d/.test(password);
+    const specialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const match = password && passwordConfirm && password === passwordConfirm;
+
+    setPasswordCriteria({ length, number, specialChar, match });
+  }, [formData, formData.password, formData.passwordConfirm]);
 
   return (
     <div className="register-container">
@@ -131,6 +165,48 @@ const Register = () => {
                   onChange={handleChange}
                 />
               </div>
+              <Box sx={{ ml: 8 }}>
+                <Typography
+                  fontSize="12px"
+                  sx={{
+                    color: passwordCriteria.length
+                      ? colors.greenAccent[500]
+                      : colors.redAccent[500],
+                  }}
+                >
+                  Contains at least 8 characters
+                </Typography>
+                <Typography
+                  fontSize="12px"
+                  sx={{
+                    color: passwordCriteria.number
+                      ? colors.greenAccent[500]
+                      : colors.redAccent[500],
+                  }}
+                >
+                  Contains at least one number
+                </Typography>
+                <Typography
+                  fontSize="12px"
+                  sx={{
+                    color: passwordCriteria.specialChar
+                      ? colors.greenAccent[500]
+                      : colors.redAccent[500],
+                  }}
+                >
+                  Contains at least one special character
+                </Typography>
+                <Typography
+                  fontSize="12px"
+                  sx={{
+                    color: passwordCriteria.match
+                      ? colors.greenAccent[500]
+                      : colors.redAccent[500],
+                  }}
+                >
+                  Password and confirm password match
+                </Typography>
+              </Box>
             </div>
 
             <div className="submit-container">
--- stonksbro-app/src/components/UpdatePassword/UpdatePassword.js
+++ stonksbro-app/src/components/UpdatePassword/UpdatePassword.js
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import MessageBox from "../MessageBox/MessageBox";
 
+import { tokens } from "../../theme";
+import { useTheme, Box, Typography } from "@mui/material";
+
 import password_icon from "../Assets/password.png";
 import logo_icon from "../Assets/stonksBro-icon.png";
 import stock_image from "../Assets/stocksimage.jpg";
 
 const UpdatePassword = () => {
+  const colorTheme = useTheme();
+  const colors = tokens(colorTheme.palette.mode);
+
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const navigate = useNavigate();
@@ -37,8 +43,13 @@ const UpdatePassword = () => {
       return;
     }
 
-    if (formData.password !== formData.passwordConfirm) {
-      setErrorMessage("Passwords do not match.");
+    if (
+      !passwordCriteria.length ||
+      !passwordCriteria.number ||
+      !passwordCriteria.specialChar ||
+      !passwordCriteria.match
+    ) {
+      setErrorMessage("Please make sure all password requirements are met.");
       return;
     }
 
@@ -72,6 +83,24 @@ const UpdatePassword = () => {
       setSuccessMessage("");
     }
   };
+
+  const [passwordCriteria, setPasswordCriteria] = useState({
+    length: false,
+    number: false,
+    specialChar: false,
+    match: false,
+  });
+
+  useEffect(() => {
+    const { password, passwordConfirm } = formData;
+
+    const length = password.length >= 8;
+    const number = /\d/.test(password);
+    const specialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const match = password && passwordConfirm && password === passwordConfirm;
+
+    setPasswordCriteria({ length, number, specialChar, match });
+  }, [formData, formData.password, formData.passwordConfirm]);
 
   return (
     <div className="update-container">
@@ -110,6 +139,48 @@ const UpdatePassword = () => {
                   onChange={handleChange}
                 />
               </div>
+              <Box sx={{ ml: 8 }}>
+                <Typography
+                  fontSize="12px"
+                  sx={{
+                    color: passwordCriteria.length
+                      ? colors.greenAccent[500]
+                      : colors.redAccent[500],
+                  }}
+                >
+                  Contains at least 8 characters
+                </Typography>
+                <Typography
+                  fontSize="12px"
+                  sx={{
+                    color: passwordCriteria.number
+                      ? colors.greenAccent[500]
+                      : colors.redAccent[500],
+                  }}
+                >
+                  Contains at least one number
+                </Typography>
+                <Typography
+                  fontSize="12px"
+                  sx={{
+                    color: passwordCriteria.specialChar
+                      ? colors.greenAccent[500]
+                      : colors.redAccent[500],
+                  }}
+                >
+                  Contains at least one special character
+                </Typography>
+                <Typography
+                  fontSize="12px"
+                  sx={{
+                    color: passwordCriteria.match
+                      ? colors.greenAccent[500]
+                      : colors.redAccent[500],
+                  }}
+                >
+                  Password and confirm password match
+                </Typography>
+              </Box>
             </div>
 
             <div className="submit-container">
